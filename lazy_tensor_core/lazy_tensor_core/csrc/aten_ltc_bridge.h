8bdda59dda0e3b1be61520edc8fc35a30c14f8a3
Will Constable <whc@fb.com>
2021-11-04 13:28:37 -0700

    WIP iterate on the backend API for device mgmt
    Refactor backend to new interface
    
    Refactor bridge::GetLtcDevice and fix bug in backend
    
    Add EagerFallbackDeviceType()

--- lazy_tensor_core/lazy_tensor_core/csrc/aten_ltc_bridge.h
+++ lazy_tensor_core/lazy_tensor_core/csrc/aten_ltc_bridge.h
@@ -49,31 +49,17 @@ void LtcUpdateTensors(c10::ArrayRef<at::Tensor> dest_ltc_tensors,
 void LtcUpdateTensorsMeta(c10::ArrayRef<at::Tensor> dest_ltc_tensors,
                           c10::ArrayRef<at::Tensor> source_cpu_tensors,
                           c10::ArrayRef<size_t> indices);
-
-// Tries to extract the device out of the lazy tensor. Returns nullopt if the
-// input is not a lazy tensor.
-c10::optional<torch::lazy::BackendDevice> GetLtcDevice(const at::Tensor& tensor);
-
-c10::optional<torch::lazy::BackendDevice> GetLtcDevice(const c10::optional<at::Tensor>& tensor);
-
-c10::optional<torch::lazy::BackendDevice> GetLtcDevice(const at::TensorList& tensors);
-
-c10::optional<torch::lazy::BackendDevice> GetLtcDevice(const at::TensorOptions& tensor_options);
-
-c10::optional<torch::lazy::BackendDevice> GetLtcDevice(const c10::Device& device);
-
-c10::optional<torch::lazy::BackendDevice> GetLtcDevice(
-    const c10::optional<c10::Device>& device = c10::nullopt);
-
+c10::optional<torch::lazy::BackendDevice> GetSameBackendDeviceOrUseDefault();
+c10::optional<torch::lazy::BackendDevice> GetSameBackendDeviceOrUseDefault(const at::Tensor& tensor);
+c10::optional<torch::lazy::BackendDevice> GetSameBackendDeviceOrUseDefault(const at::TensorList& tensors);
 template<typename T, typename... Args>
-c10::optional<torch::lazy::BackendDevice> GetLtcDevice(const T& tensor, const Args&... forward_tensors) {
-    auto optional_device = GetLtcDevice(tensor);
+c10::optional<torch::lazy::BackendDevice> GetSameBackendDeviceOrUseDefault(const T& tensor, const Args&... forward_tensors) {
+    auto optional_device = GetSameBackendDeviceOrUseDefault(tensor);
     if (optional_device) {
         return optional_device;
     }
-    return GetLtcDevice(forward_tensors...);
+    return GetSameBackendDeviceOrUseDefault(forward_tensors...);
 }
-
 torch::lazy::BackendDevice AtenDeviceToLtcDevice(const c10::Device& device);
 
 c10::Device LtcDeviceToAtenDevice(const torch::lazy::BackendDevice& device);
